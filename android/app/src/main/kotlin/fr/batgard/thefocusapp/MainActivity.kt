2acda0a86266973397091e9e1cc524831707c647
Batgard <remi.gatard@gmail.com>
2020-04-03 16:57:34 +0200

    Add all generated files

---
+++ android/app/src/main/kotlin/fr/batgard/thefocusapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr.batgard.thefocusapp
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
